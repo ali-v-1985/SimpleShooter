33f942ab9b95a762e01fbce02349422aff2c1c58
Ali <ali.valizadeh.h@gmail.com>
2020-10-05 01:32:18 +0200

    Source code and bp of the character and actors are added

---
+++ Source/SimpleShooter/Characters/ShooterCharacter.cpp
@@ -0,0 +1,170 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterCharacter.h"
+#include "Components/InputComponent.h"
+#include "Engine/World.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "SimpleShooter/Actors/Combat/GunBase.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AShooterCharacter::AShooterCharacter()
+{
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+
+    SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+    SpringArmComponent->SetupAttachment(RootComponent);
+
+    CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    CameraComponent->SetupAttachment(SpringArmComponent);
+}
+
+void AShooterCharacter::SpawnWeapons()
+{
+    for(auto WeaponType : WeaponInventory)
+    {
+        const auto Weapon = GetWorld()->SpawnActor<AGunBase>(WeaponType);
+        if (WeaponOfChoice == WeaponType)
+        {
+            WeaponInUse = Weapon;
+        }
+    }
+}
+
+void AShooterCharacter::UseChoseWeapon(AGunBase* Weapon)
+{
+    Weapon->AttachToComponent(GetMesh(),
+                                   FAttachmentTransformRules::KeepRelativeTransform,
+                                   TEXT("WeaponSocket"));
+    Weapon->SetOwner(this);
+}
+
+void AShooterCharacter::HideDefaultWeapon() const
+{
+    GetMesh()->HideBoneByName(TEXT("weapon_r"), PBO_None);
+}
+
+// Called when the game starts or when spawned
+void AShooterCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+    SpawnWeapons();
+    HideDefaultWeapon();
+    UseChoseWeapon(WeaponInUse);
+}
+
+// Called every frame
+void AShooterCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    ChangeHand();
+}
+
+// Called to bind functionality to input
+void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AShooterCharacter::MoveForward);
+    PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AShooterCharacter::MoveRight);
+    PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &AShooterCharacter::AddControllerPitchInput);
+    PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &AShooterCharacter::AddControllerYawInput);
+    PlayerInputComponent->BindAxis(TEXT("LookUpRate"), this, &AShooterCharacter::LookUp);
+    PlayerInputComponent->BindAxis(TEXT("LookRightRate"), this, &AShooterCharacter::LookRight);
+    PlayerInputComponent->BindAction(TEXT("Jump"), IE_Pressed, this, &AShooterCharacter::Jump);
+    PlayerInputComponent->BindAction(TEXT("ChangeHand"), IE_Pressed, this, &AShooterCharacter::ChangeHandDelegate);
+    PlayerInputComponent->BindAction(TEXT("Fire"), IE_Pressed, this, &AShooterCharacter::PullTrigger);
+}
+
+void AShooterCharacter::PullTrigger()
+{
+    WeaponInUse->Fire();
+}
+
+void AShooterCharacter::MoveForward(float Value)
+{
+    AddMovementInput(GetActorForwardVector() * Value);
+}
+
+void AShooterCharacter::MoveRight(float Value)
+{
+    AddMovementInput(GetActorRightVector() * Value);
+}
+
+void AShooterCharacter::LookUp(float Value)
+{
+    AddControllerPitchInput(Value * RotationRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AShooterCharacter::LookRight(float Value)
+{
+    AddControllerYawInput(Value * RotationRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AShooterCharacter::ChangeHandDelegate()
+{
+    ShouldChangeHand = true;
+
+    UE_LOG(LogTemp, Warning, TEXT("SpringArmComponent.RelativeLocation.Y before moving is %f"), SpringArmComponent->GetRelativeLocation().Y);
+    UE_LOG(LogTemp, Warning, TEXT("SpringArmComponent.SocketOffset.Y before moving is %f"), SpringArmComponent->SocketOffset.Y);
+        
+    TargetRelativeLocationY = SpringArmComponent->GetRelativeLocation().Y * -1;
+    TargetSocketOffsetY = SpringArmComponent->SocketOffset.Y * -1;
+
+    UE_LOG(LogTemp, Warning, TEXT("TargetRelativeLocation.Y is %f"), TargetRelativeLocationY);
+    UE_LOG(LogTemp, Warning, TEXT("TargetSocketOffset.Y is %f"), TargetSocketOffsetY);
+        
+    
+}
+
+void AShooterCharacter::ChangeHand()
+{
+    if (SpringArmComponent && ShouldChangeHand)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("ChangeHand is being executed"));
+        ChangeSpringArmRelativeLocationY();
+
+        ChangeSpringArmSocketOffsetY();
+
+        if(FMath::IsNearlyEqual(SpringArmComponent->GetRelativeLocation().Y, TargetRelativeLocationY, 1.0f) 
+            && FMath::IsNearlyEqual(SpringArmComponent->SocketOffset.Y, TargetSocketOffsetY, 1.0f))
+        {
+            ShouldChangeHand = false;
+            auto RelativeLocation = SpringArmComponent->GetRelativeLocation();
+            RelativeLocation.Y = TargetRelativeLocationY;
+            SpringArmComponent->SetRelativeLocation(RelativeLocation);
+            SpringArmComponent->SocketOffset.Y = TargetSocketOffsetY;
+            UE_LOG(LogTemp, Warning, TEXT("SpringArmComponent.RelativeLocation.Y after moving is %f"), SpringArmComponent->GetRelativeLocation().Y);
+            UE_LOG(LogTemp, Warning, TEXT("SpringArmComponent.SocketOffset.Y after moving is %f"), SpringArmComponent->SocketOffset.Y);
+        }
+    }
+}
+
+void AShooterCharacter::ChangeSpringArmRelativeLocationY() const
+{
+    auto SpringArmRelativeLocation = SpringArmComponent->GetRelativeLocation();
+    const auto CurrentSpringArmLocationY = FMath::FInterpTo(SpringArmRelativeLocation.Y,
+                                                            TargetRelativeLocationY, GetWorld()->GetDeltaSeconds(),
+                                                            ChangeHandSpeed);
+
+    SpringArmRelativeLocation.Y = CurrentSpringArmLocationY;
+    SpringArmComponent->SetRelativeLocation(SpringArmRelativeLocation);
+    UE_LOG(LogTemp, Warning, TEXT("SpringArmComponent.RelativeLocation.Y while moving is %f"), SpringArmComponent->GetRelativeLocation().Y);    
+}
+
+void AShooterCharacter::ChangeSpringArmSocketOffsetY() const
+{
+    auto SocketOffset = SpringArmComponent->SocketOffset;
+    const auto CurrentSpringArmSocketOffsetY = FMath::FInterpTo(SocketOffset.Y,
+                                                                TargetSocketOffsetY,
+                                                                GetWorld()->GetDeltaSeconds(),
+                                                                ChangeHandSpeed);
+
+    SocketOffset.Y = CurrentSpringArmSocketOffsetY;
+    SpringArmComponent->SocketOffset = SocketOffset;
+    UE_LOG(LogTemp, Warning, TEXT("SpringArmComponent.SocketOffset.Y while moving is %f"), SpringArmComponent->SocketOffset.Y);
+}
---
+++ Source/SimpleShooter/Characters/ShooterCharacter.h
@@ -0,0 +1,75 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class AGunBase;
+
+UCLASS()
+class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooterCharacter();
+
+protected:
+	void HideDefaultWeapon() const;
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= Camera, meta=(AllowPrivateAccess="true"))
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= Camera, meta=(AllowPrivateAccess="true"))
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(EditAnywhere, Category= Movement)
+	float RotationRate = 10;
+
+	UPROPERTY(EditAnywhere, Category= Camera)
+	float ChangeHandSpeed = 10;
+
+	UPROPERTY(EditDefaultsOnly, Category= Combat, meta=(AllowPrivateAccess="true"))
+	TArray<TSubclassOf<AGunBase>> WeaponInventory;
+
+	UPROPERTY(EditDefaultsOnly, Category= Combat, meta=(AllowPrivateAccess="true"))
+	TSubclassOf<AGunBase> WeaponOfChoice;
+	
+	// TArray<AGunBase*> Weapons;
+
+	AGunBase* WeaponInUse;
+	
+	bool ShouldChangeHand = false;
+	
+	float TargetSocketOffsetY;
+	float TargetRelativeLocationY;
+	
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void LookUp(float Value);
+	void LookRight(float Value);
+	void ChangeHandDelegate();
+	void ChangeHand();
+	void ChangeSpringArmRelativeLocationY() const;
+	void ChangeSpringArmSocketOffsetY() const;
+	void PullTrigger();
+	void SpawnWeapons();
+	void UseChoseWeapon(AGunBase* Weapon);
+};
